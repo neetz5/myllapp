79d89eb88ea06f7bfb660d195470aeba48136e03
Neetu Suresh <neetusuresh@Neetus-MBP.attlocal.net>
2023-07-23 12:40:11 -0400

    route

---
+++ src/components/About.js
@@ -0,0 +1,4 @@
+function About() {
+  return <>About Us</>;
+}
+export default About;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 function Main() {
   return (
-    <main style={{ backgroundColor: "#495E57", padding: "42vh" }}>
-      Little Lemon Restaurant
-    </main>
+    <BrowserRouter>
+      <main style={{ backgroundColor: "#495E57", padding: "42vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/aboutus" element={<About />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 export default Main;
